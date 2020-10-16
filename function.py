5df797c10413b145baa6a2167601b7506c8696b9
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-10-16 22:44:22 +0300

    append method, repr of function

--- function.py
+++ function.py
@@ -5,8 +5,8 @@ class Function:
 
     def __init__(self, domain=None, values=None, func=None, start=None, stop=None, num=None):
         self.func = func
-        self.domain = domain
-        self.values = values
+        self.domain = np.array(domain)
+        self.values = np.array(values)
         self.start = start
         self.stop = stop
         self.num = num
@@ -22,19 +22,27 @@ class Function:
         self.num = num
 
         self.domain = np.linspace(start, stop, num) 
-        return self.X
+        return self.domain
     
     def generate_Y(self):
         self.values = self.func(self.domain)
         return self.values
+    
+    def append(self, x, y):
+        self.domain = np.append(self.domain, [x])
+        self.values = np.append(self.values, [y])
+
     @property
     def X(self):
         return self.domain
     
     @property
     def Y(self):
-        return self.generate_Y()
+        return self.values
     
+    def __str__(self):
+        return 'Function (' + ','.join([self.X.__str__() , self.Y.__str__()]) + ')'
+
 if __name__ == "__main__":
     f = Function(func=lambda x: x**2)
     f.generate_X(1, 20, 100)
